d50eac274a2b069515c040503a4da405abf15c2e
ZoePorta <zoeportagarcia@gmail.com>
2020-05-26 04:19:12 +0200

    segundo commit

--- back/server.js
+++ back/server.js
@@ -5,8 +5,13 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 
-const { userIsAuthenticated, userIsAdmin } = require("./middlewares/auth");
+const {
+  userIsAuthenticated,
+  userIsAdmin,
+  userIsVendor,
+} = require("./middlewares/auth");
 
 const app = express();
 
@@ -16,10 +21,20 @@ const {
   loginUser,
   getInfoUser,
   editUser,
-  /* updatePasswordUser,*/
+  updatePasswordUser,
+  updateEmailUser,
   validateUser,
+  validateEmailUser,
   resendVerificationEmail,
 } = require("./controllers/users");
+
+const {
+  createShop,
+  editShop,
+  promoteShop,
+  unpromoteShop,
+  deleteShop,
+} = require("./controllers/shops");
 
 //Logger middleware
 app.use(morgan("dev"));
@@ -30,16 +45,42 @@ app.use(bodyParser.json());
 //Multipart parsing middleware
 app.use(fileUpload());
 
+app.use(express.static(path.join(__dirname, "static")));
+
 //Routes
 /////Users
 app.post("/users", registerUser);
 app.post("/users/login", loginUser);
 app.post("/users/resendmail", resendVerificationEmail);
+app.get("/users/validate", validateUser);
+app.get("/users/validateEmail", validateEmailUser);
 app.get("/users/:id", userIsAuthenticated, getInfoUser);
 app.put("/users/:id", userIsAuthenticated, editUser);
-/*app.put("/users/:id/password", updatePasswordUser);*/
-app.get("/users/:id/validate", validateUser);
+app.put("/users/:id/password", userIsAuthenticated, updatePasswordUser);
+app.put("/users/:id/email", userIsAuthenticated, updateEmailUser);
 
+/////Products
+//app.get("/products", listProducts);
+app.post("/products", userIsAuthenticated, userIsVendor);
+//app.get("/products/:id", getProduct);
+//app.post("/products/:id/rate", userIsAuthenticated, rateProduct);
+
+/////Shops
+app.post("/shops", userIsAuthenticated, createShop);
+app.put("/shops/:shopId", userIsAuthenticated, userIsVendor, editShop);
+app.put(
+  "/shops/:shopId/promote",
+  userIsAuthenticated,
+  userIsAdmin,
+  promoteShop
+);
+app.put(
+  "/shops/:shopId/unpromote",
+  userIsAuthenticated,
+  userIsAdmin,
+  unpromoteShop
+);
+app.delete("/shops/:shopId", userIsAuthenticated, userIsVendor, deleteShop);
 //Error middleware
 app.use((error, req, res, next) => {
   console.log(error);
